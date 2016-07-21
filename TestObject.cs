4458be108529d43c8f5044ef22c7c8b39d791aeb
Berkley Bernales <Berkley@Berks-MacBook-Pro.local>
2016-07-22 00:46:56 +0800

    Initial commit

---
+++ TestObject.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XamTest
+{
+	public class TestObject
+	{
+		public TestObject()
+		{
+			Test1 = 5;
+			Test2 = "berksGwapo";
+			Test3 = 9.5f;
+			Test4 = new Dictionary<string,object> []
+			{ 
+				new Dictionary<string,object>(),
+				new Dictionary<string,object>()
+			};
+
+			Test4[0].Add("test5", "test 5");
+			Test4[1].Add("test6", "test 6");
+		}
+
+		int test1;
+		string test2;
+		float test3;
+		Dictionary<string, object>[] test4;
+
+		public int Test1
+		{
+			get
+			{
+				return test1;
+			}
+
+			set
+			{
+				test1 = value;
+			}
+		}
+
+		public string Test2
+		{
+			get
+			{
+				return test2;
+			}
+
+			set
+			{
+				test2 = value;
+			}
+		}
+
+		public float Test3
+		{
+			get
+			{
+				return test3;
+			}
+
+			set
+			{
+				test3 = value;
+			}
+		}
+
+		public Dictionary<string, object>[] Test4
+		{
+			get
+			{
+				return test4;
+			}
+
+			set
+			{
+				test4 = value;
+			}
+		}
+		//Dictionary<string,object>[] test4;
+	}
+}
+
---
+++ TrioTools.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Motion.Mobile.Core.BLE;
+
+//[assembly: XamlCompilationAttribute(XamlCompilationOptions.Compile)]
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Motion.Misc.Utilities;
+using Motion.Core.WSHandler;
+using Motion.Core.SyncHandler;
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using Acrotech.PortableLogAdapter;
+using Acrotech.PortableLogAdapter.Managers;
+
+namespace XamTest
+{
+	public class App : Application
+	{
+		static IAdapter Adapter;
+		TestPage testPage;
+
+		public static readonly ILogManager LogManager = new DelegateLogManager((logger, message) => System.Diagnostics.Debug.WriteLine("[{0}]{1}", logger.Name, message), LogLevel.Info);
+
+		//File file;
+		public App ()
+		{
+			TestObject test = new TestObject();
+			string testStr = JsonConvert.SerializeObject(test);
+			Debug.WriteLine(testStr);
+
+			ILogger logger = LogManager.GetLogger("TestLogger");
+
+			logger.Info("This is a test log for multi-platform");
+
+			testPage = new TestPage(Adapter);
+			MainPage = new NavigationPage(testPage) {
+				BarBackgroundColor = Color.Yellow,
+				BarTextColor = Color.Blue
+			};
+		}
+
+		public static void SetAdapter (IAdapter adapter) {
+			Adapter = adapter;
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+			this.testPage.StartScanning(0x180F.UuidFromPartial());
+
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
